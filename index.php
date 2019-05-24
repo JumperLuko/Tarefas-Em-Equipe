7343b8f7f56f33d022c27bd03ebd2601daa0eb2b
Jumper Luko <jumper.luko@gmail.com>
2019-05-24 14:22:34 -0300

    Primeira página criada

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- all METAs and LINKs in https://gist.github.com/lancejpollard/1978404 -->
+		<meta charset="UTF-8">
+		<meta name="language" content="pt-br,en-us">
+		<meta property="og:locale" content="pt_BR">
+		<!-- <link rel="icon" type="image/png" sizes="32x32" href=""> -->
+		<!-- <link rel="shortcut icon" type="image/png" sizes="64x64" href=""> -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Tarefas em Equipes</title>
+		
+		<!-- <meta name="description" content="150 words"> -->
+		<!-- <meta property="og:description" content="150 words"> -->
+		<!-- <meta property="og:site_name" content="Site Title"> -->
+		<!-- <meta property="og:title" content="Page Title"> -->
+		<!-- <meta name="theme-color" content="#ffffff"> -->
+		<!-- <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff"> -->
+		<!-- <meta property="og:type" content="website or article"> -->
+		<!-- <meta property="article:section" content="Tutorials"> -->
+		<!-- <meta property="article:tag" content="Facebook, tags, og, open graph"> -->
+		<!-- <meta property="article:published_time" content="date_time"> -->
+		<!-- <meta property="og:image" content="https://mysite.com/img/image.jpg"> -->
+		<!-- <meta property="og:image:type" content="image/jpeg"> -->
+		<!-- <meta property="og:image:width" content="800"> /** PIXELS **/ -->
+		<!-- <meta property="og:image:height" content="600"> /** PIXELS **/ -->
+		<!-- <meta property="og:url" content="https://example.com/page.html"> -->
+		<!-- <meta name="twitter:card" content="summary_large_image"> -->
+		<!-- <meta name="author" content="Jumper_Luko, jumper.luko@gmail.com"> -->
+		<!-- <meta property="article:author" content="Jumper_Luko, jumper.luko@gmail.com"> -->
+		<!-- <meta name="owner" content="Jumper_Luko, jumper.luko@gmail.com"> -->
+		<!-- <meta name="designer" content="Jumper_Luko, jumper.luko@gmail.com"> -->
+		<!-- <meta name="generator" content="Sublime,Geany,VSCode...."> -->
+		
+		<!-- <meta name="robots" content="index, follow"> -->
+		<!-- <meta name="keywords" content="Words, keys, tags"> -->
+		<!-- <meta name="rating" content="general"> -->
+		<!-- <meta name="revised" content="Sunday, July 18th, 2010, 5:15 pm" /> -->
+		<!-- <meta http-equiv="Pragma" content="no-cache"> -->
+		<!-- <meta http-equiv="Expires" content="0 days"> -->
+		<!-- <meta name="url" content="http://www.websiteaddrress.com"> -->
+		
+		<link rel="stylesheet" type="text/css" href="./css/responsive_flex-grid.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/style.min.css">
+		<!-- <link rel="stylesheet" type="text/css" href=""> -->
+		<!-- <link rel="stylesheet" type="text/css" href="" media="only screen and (max-device-width: 480px)" /> -->
+		<!-- <script type="text/javascript" src=""></script> -->
+	</head>
+	<body>
+        <nav class="flex betweenH centerV h4 centerText">
+            <a>Nick: Fulano</a>
+            <a>Tarefas: 20 - requisições: 5</a>
+            <a>Logout</a>
+        </nav>
+        <main class="gcol-4 grow-3 grid wrap menuInicial centerText h2">
+            <a class="m1">Tarefas</a>
+			<a class="m3">Tarefas da Equipe</a>
+			<a class="m5">Minhas Tarefas</a>
+			<a class="m6">Minhas Responsabilidades</a>
+            <a class="m2">Murais</a>
+            <a class="m4">Mural da Equipe</a>
+            <a class="m7">Meu Mural</a>
+        </main>
+    </body>
+</html>
